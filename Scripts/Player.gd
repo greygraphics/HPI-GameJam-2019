2baa217470e03ad14be900f291a61864f475fae8
Philipp Keese <philipp.keese@student.hpi.uni-potsdam.de>
2019-11-16 00:19:38 +0100

    Implemented falling

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,22 +1,37 @@
 extends KinematicBody2D
 
-export var speed : float = 1
+export var accellearation : float = 5
+export var maxSpeed : float = 100
+export var slideFactor : float = 0.8
 export var gravity : float = 100
 export var jumpHeight : float = 100
-export var wallBreak : float = 50
+export var jumpSpeedFactor : float = 0.2
 
 var velocity = Vector2()
+var isFalling = true
 
 func _physics_process(delta):
-	var gravityFactor = 1
-	velocity.x += Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	# Calculate X movement
+	var hasXInput = false
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = min(velocity.x + accellearation, maxSpeed)
+		hasXInput = true
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = max(velocity.x - accellearation, -maxSpeed)
+		hasXInput = true
+	if !hasXInput:
+		velocity.x *= slideFactor
+		
+	# Calcualte Y movement
+	var gravityScale = 1
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = -jumpHeight
-	if Input.is_action_pressed("ui_up"):
-		gravityFactor *= 0.5
-	if is_on_wall():
-		gravityFactor *= 0.5
+	isFalling = velocity.y > 0
+	# Let player "float" if holding up
+	if Input.is_action_pressed("ui_up") and isFalling:
+		gravityScale = 0.5
+	velocity.y += gravity * gravityScale * delta
 	
-	velocity.y += delta * gravity * gravityFactor;
-	velocity = move_and_slide(velocity, Vector2(0,-1))
+	# Commit new velocity
+	velocity = move_and_slide(velocity, Vector2(0, -1))
