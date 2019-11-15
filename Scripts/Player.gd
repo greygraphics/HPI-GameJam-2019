fce72b6e6d2a86723ead21ac1f8891fca4ab5a6e
Philipp Keese <philipp.keese@student.hpi.uni-potsdam.de>
2019-11-15 21:00:22 +0100

    Added Player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,20 +1,15 @@
 extends RigidBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var speed : float = 1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _process(delta):
+	linear_velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
 func _input(event):
-	if event is InputEventKey:
-		moveplayer(event)
-		
-func moveplayer(event):
-	pass
+	print("Input")
+	print(Input.get_action_strength("ui_left"))
