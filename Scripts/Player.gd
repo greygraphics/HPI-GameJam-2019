be923ab120d82ddfcd3df8b788889abb8c42aded
Philipp Keese <philipp.keese@student.hpi.uni-potsdam.de>
2019-11-16 00:34:13 +0100

    Branched jumping and walljumping

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export var accellearation : float = 5
+export var accellearation : float = 50
 export var maxSpeed : float = 100
 export var slideFactor : float = 0.8
 export var gravity : float = 100
@@ -24,17 +24,25 @@ func _physics_process(delta):
 		
 	# Calcualte Y movement
 	var gravityScale = 1
+	isFalling = velocity.y > 0
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = -jumpHeight
-	isFalling = velocity.y > 0
-	# Let player "float" if holding up
-	if Input.is_action_pressed("ui_up") and isFalling:
-		gravityScale = 0.5
+		# Let player "float" if holding up
+		elif Input.is_action_pressed("ui_up") and isFalling:
+			gravityScale = 0.5
+		# Calculate walljumps
+	elif Input.is_action_pressed("ui_left"):
+		if $wallRays/rayLeft.is_colliding():
+			if Input.is_action_just_pressed("ui_up"):
+				velocity.y -= jumpHeight
+				velocity.x += accellearation
+		if $wallRays/rayRight.is_colliding():
+			if Input.is_action_just_pressed("ui_up"):
+				velocity.y -= jumpHeight
+				velocity.x -= accellearation
 	velocity.y += gravity * gravityScale * delta
-	
-	# Calculate walljumps
-	
+		
 	
 	# Commit new velocity
 	velocity = move_and_slide(velocity, Vector2(0, -1))
