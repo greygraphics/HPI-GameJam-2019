e42f14259a4c42929efc751b8761d1e01fd39bb6
Philipp Keese <philipp.keese@student.hpi.uni-potsdam.de>
2019-11-16 12:52:58 +0100

    Fixed Node type

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,10 +5,11 @@ export var maxSpeed : float = 100
 export var slideFactor : float = 0.8
 export var gravity : float = 100
 export var jumpHeight : float = 100
-export var jumpSpeedFactor : float = 0.2
+export var jumpSpeedFactor : float = 0.5
 
 var velocity = Vector2()
 var isFalling = true
+var moveDirection = 0
 
 func _physics_process(delta):
 
@@ -16,21 +17,22 @@ func _physics_process(delta):
 		print("Collision left!");
 		
 	# Calculate X movement
-	var hasXInput = false
+	moveDirection = 0
+	var correctedAcc = accellearation * _getXAccFactor()
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = min(velocity.x + accellearation, maxSpeed)
-		hasXInput = true
+		velocity.x = min(velocity.x + correctedAcc, maxSpeed)
+		moveDirection += 1
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = max(velocity.x - accellearation, -maxSpeed)
-		hasXInput = true
-	if !hasXInput:
+		velocity.x = max(velocity.x - correctedAcc, -maxSpeed)
+		moveDirection -= 1
+	if moveDirection == 0:
 		velocity.x *= slideFactor
 		
 	# Calcualte Y movement
 	# Jumping
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
-			velocity.y -= jumpHeight
+			velocity.y -= jumpHeight + (jumpHeight * abs(velocity.x) / maxSpeed * jumpSpeedFactor)
 		elif _check_rays($wallRays):
 			_walljump()
 	velocity.y += gravity * delta
@@ -52,3 +54,9 @@ func _walljump():
 		velocity.x += accellearation
 	else:
 		velocity.x -= accellearation
+		
+func _getXAccFactor():
+	if is_on_floor():
+		return 1
+	else:
+		return 0.1
