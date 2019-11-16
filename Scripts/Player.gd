ec410e42dbb6c17a3ba1c27cea772d7a38709882
Philipp Keese <philipp.keese@student.hpi.uni-potsdam.de>
2019-11-16 14:38:16 +0100

    Rewrote X movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,82 +1,79 @@
 extends KinematicBody2D
 
-export var accellearation : float = 50
-export var maxSpeed : float = 100
-export var slideFactor : float = 0.8
-export var gravity : float = 100
-export var jumpHeight : float = 100
-export var jumpSpeedFactor : float = 0.5
-export var wallJumpSpeed : Vector2 = Vector2()
-export var wallSlideBoost : float = 50
+export var MAX_SPEED : float = 8
+export var ACCELLERATION : float = 1
+export var AIR_ACC_FACTOR : float = 0.5
+export var SLIDE_FACTOR : float = 0.8
+export var JUMP_HEIGHT : float = 16
+export var WALLSLIDE_FACTOR : float = 0.5
+export var GRAVITY : float = 8
 
+enum STATES {
+	FLOOR,
+	AIR,
+	WALL
+}
 
-var velocity = Vector2()
-var isFalling = true
-var moveDirection = 0
+var velocity : Vector2 = Vector2()
 var wallDir = 0
-
-onready var leftRay = $rayLeft
-onready var rightRay = $rayRight
-
+var moveDir = 0
+var state = STATES.FLOOR
 
 
 func _physics_process(delta):
-	# Calculate X movement
-	moveDirection = 0
-	var correctedAcc = accellearation * _getXAccFactor()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = min(velocity.x + correctedAcc, maxSpeed)
-		moveDirection += 1
+	# Get X Input
+	_updateMoveDir()
+	# Get accelleration factor
+	var accFactor = 1
+	match state:
+		STATES.AIR:
+			accFactor = AIR_ACC_FACTOR
+	# Break if nothing pressed
+	if moveDir == 0:
+		velocity.x *= SLIDE_FACTOR
+	# Move player on X
+	velocity.x += moveDir * accFactor * ACCELLERATION
+	
+	_checkWalls()
+	# Get Y input
+	# jump
+	_jump()
+	_walljump()
+	
+	var gravityScale = 1
+	# wallslide
+	gravityScale *= _wallslide()
+	# hover
+	# apply gravity
+	velocity.y += gravityScale * GRAVITY
+	
+	# apply movement
+	velocity = move_and_slide(velocity, Vector2.DOWN)
+	pass
+	
+func _updateMoveDir():
+	moveDir = 0
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = max(velocity.x - correctedAcc, -maxSpeed)
-		moveDirection -= 1
-	if moveDirection == 0:
-		velocity.x *= slideFactor
-		
-	# Calcualte Y movement
-	# Jumping
-	if Input.is_action_just_pressed("ui_up"):
-		if is_on_floor():
-			velocity.y -= jumpHeight + (jumpHeight * abs(velocity.x) / maxSpeed * jumpSpeedFactor)
-		elif _check_rays():
-			_walljump()
-	
-	# Calculate wall slide
-	if wallDir != 0:
-		_wallslide()
-	
-	
-	
-	velocity.y += gravity
-		
-	
-	# Commit new velocity
-	velocity = move_and_slide(velocity, Vector2(0, -1))
-	
-func _check_rays():
-	wallDir = 0
-	if leftRay.is_colliding():
-		wallDir -= 1
-		return true
-	if rightRay.is_colliding():
-		wallDir += 1
-		return true
-	return false
+		moveDir -= 1
+	if Input.is_action_pressed("ui_right"):
+		moveDir += 1
 
-func _walljump():
-	print("Walljump")
-	velocity += Vector2(wallJumpSpeed.x * wallDir, wallJumpSpeed.y)
-	
-func _wallslide():
-	if Input.is_action_pressed("ui_left") and wallDir < 0:
-		velocity.y -= wallSlideBoost
-	if Input.is_action_pressed("ui_right") and wallDir > 0:
-		velocity.y -= wallSlideBoost
-	
-	
+
+func _checkWalls():
+	wallDir = 0
+	if $rayLeft.is_colliding():
+		wallDir -= 1
+	if $rayRight.is_colliding():
+		wallDir += 1
 		
-func _getXAccFactor():
-	if is_on_floor():
-		return 1
-	else:
-		return 0.1
+func _wallslide():
+	if moveDir == wallDir:
+		return WALLSLIDE_FACTOR
+	return 1
+	
+func _jump():
+	pass
+	
+func _walljump():
+	pass
+	
